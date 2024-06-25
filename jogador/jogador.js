3eb4a65a2aad489bec8b6d7cca72d924205eb7fc
PedroFRSx <pedro-souza-reis@outlook.com>
2024-06-25 02:58:25 -0300

    Finalizando JS para os métodos tanto em time quanto jogadores

---
+++ jogador/jogador.js
@@ -0,0 +1,200 @@
+
+$(document).ready(function() {
+    // Ao carregar a página, listar os jogadores existentes
+    listarJogadores();
+    // Carregar times para preencher o select
+    carregarTimes();
+
+    // Limpar modal ao abrir
+    $("#myModal").on("show.bs.modal", function() {
+        limparModal();
+    });
+
+    // Ação do botão salvar no modal
+    $("#salvarBotao").click(function(event) {
+        event.preventDefault();
+
+        var jogador = {
+            'id': $("#jogadorId").val(), // Adiciona o ID do jogador ao objeto
+            'nome': $("#nome").val(),
+            'salario': $("#salario").val(),
+            'posicao': $("#posicao").val(),
+            'dataNascimento': $("#dataNascimento").val(),
+            'velocidade': $("#velocidade").val(),
+            'forca': $("#forca").val(),
+            'altura': $("#altura").val(),
+            'peso': $("#peso").val(),
+            'desempenho': $("#desempenho").val(),
+            'time': {
+                'id': $("#idTime").val()
+            }
+        };
+
+        if (!jogador.id) {
+            cadastrarJogador(jogador);
+        } else {
+            editarJogador(jogador);
+        }
+    });
+});
+
+// Função para listar jogadores na tabela
+function listarJogadores() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/jogadores",
+        success: function(response) {
+            $("#corpoTabela").empty();
+
+            response.forEach(function(jogador) {
+                var timeId = jogador.time ? jogador.time.id : '';
+
+                var newRow = `<tr>
+                                <td>${jogador.nome}</td>
+                                <td>${jogador.salario}</td>
+                                <td>${jogador.posicao}</td>
+                                <td>${jogador.dataNascimento}</td>
+                                <td>${jogador.velocidade}</td>
+                                <td>${jogador.forca}</td>
+                                <td>${jogador.altura}</td>
+                                <td>${jogador.peso}</td>
+                                <td>${jogador.desempenho}</td>
+                                <td>${timeId}</td>
+                                <td>
+                                    <button type="button" class="btn btn-warning btn-sm" onclick="editarJogadorModal(${jogador.id})">Editar</button>
+                                    <button type="button" class="btn btn-danger btn-sm" onclick="excluirJogador(${jogador.id})">Excluir</button>
+                                </td>
+                            </tr>`;
+                $("#corpoTabela").append(newRow);
+            });
+
+            // Inicializar DataTable
+            if ($.fn.DataTable.isDataTable("#tabelaJogadores")) {
+                $('#tabelaJogadores').DataTable().destroy();
+            }
+
+            $('#tabelaJogadores').DataTable();
+
+        },
+        error: function() {
+            alert("Erro ao listar jogadores.");
+        }
+    });
+}
+
+// Função para cadastrar jogador
+function cadastrarJogador(jogador) {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/api/jogadores",
+        contentType: "application/json",
+        data: JSON.stringify(jogador),
+        success: function() {
+            $("#myModal").modal("hide");
+            listarJogadores();
+            alert("Jogador cadastrado com sucesso!");
+        },
+        error: function() {
+            alert("Erro ao cadastrar jogador.");
+        }
+    });
+}
+
+// Função para editar jogador
+function editarJogador(jogador) {
+    console.log("Jogador a ser atualizado:", jogador); // Verifica o objeto jogador antes de enviar a requisição PUT
+
+    $.ajax({
+        type: "PUT",
+        url: `http://localhost:8080/api/jogadores/${jogador.id}`,
+        contentType: "application/json",
+        data: JSON.stringify(jogador),
+        success: function() {
+            $("#myModal").modal("hide");
+            listarJogadores();
+            alert("Jogador atualizado com sucesso!");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR.responseText); // Exibe detalhes do erro no console para depuração
+            alert("Erro ao atualizar jogador.");
+        }
+    });
+}
+
+// Função para carregar jogador para edição no modal
+function editarJogadorModal(id) {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/api/jogadores/${id}`,
+        success: function(jogador) {
+            $("#myModal").modal("show");
+            $("#jogadorId").val(jogador.id); // Preenche o input hidden com o ID do jogador
+            $("#nome").val(jogador.nome);
+            $("#salario").val(jogador.salario);
+            $("#posicao").val(jogador.posicao);
+            $("#dataNascimento").val(jogador.dataNascimento);
+            $("#velocidade").val(jogador.velocidade);
+            $("#forca").val(jogador.forca);
+            $("#altura").val(jogador.altura);
+            $("#peso").val(jogador.peso);
+            $("#desempenho").val(jogador.desempenho);
+            $("#idTime").val(jogador.time.id); // Define o ID do time selecionado no select
+        },
+        error: function() {
+            alert("Erro ao carregar jogador para edição.");
+        }
+    });
+}
+
+// Função para excluir jogador
+function excluirJogador(id) {
+    if (confirm("Deseja realmente excluir este jogador?")) {
+        $.ajax({
+            type: "DELETE",
+            url: `http://localhost:8080/api/jogadores/${id}`,
+            success: function() {
+                listarJogadores();
+                alert("Jogador excluído com sucesso!");
+            },
+            error: function() {
+                alert("Erro ao excluir jogador.");
+            }
+        });
+    }
+}
+
+// Função para limpar os campos do modal
+function limparModal() {
+    $("#jogadorId").val("");
+    $("#nome").val("");
+    $("#salario").val("");
+    $("#posicao").val("");
+    $("#dataNascimento").val("");
+    $("#velocidade").val("");
+    $("#forca").val("");
+    $("#altura").val("");
+    $("#peso").val("");
+    $("#desempenho").val("");
+    $("#idTime").val("");
+}
+
+// Função para carregar os times
+function carregarTimes() {
+    $.ajax({
+        url: 'http://localhost:8080/api/times',
+        type: 'GET',
+        dataType: 'json',
+        success: function(result) {
+            var select = $("#idTime");
+            select.empty();
+            select.append('<option value="" disabled selected>Selecione um time</option>');
+
+            $.each(result, function(_i, data) {
+                select.append('<option value="' + data.id + '">' + data.nome + '</option>');
+            });
+        },
+        error: function() {
+            console.log('Erro ao carregar os times');
+        }
+    });
+}
--- time/time.js
+++ time/time.js
@@ -1,126 +1,146 @@
-function listarTimes() {
-    $.ajax({
-        url: 'http://localhost:8080/api/times',
-        type: 'GET',
-        dataType: 'json',
-        success: function (result) {
-            console.log(result);
-            var html = '';
-            $.each(result, function (_i, data) {
-                html += `<tr>`;
-                html += `<td>${data.nome}</td>`;
-                html += `<td>${data.dataCriacao}</td>`;
-                html += `<td>${data.cidade}</td>`;
-                html += `<td>${data.pais}</td>`;
-                html += `<td>${data.numeroTorcedores}</td>`;
-                html += `<td>${data.tecnico}</td>`;
-                html += `<td>${data.dinheiroCaixa}</td>`;
-                html += `<td>${data.nomeEstadio}</td>`;
-                html += `<td>`;
-                html += `<button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#myModal" onclick="preencherModal('${encodeURIComponent(JSON.stringify(data))}', ${data.id})">Editar</button>`;
-                html += `<button class="btn btn-sm btn-danger ml-1" onclick="removerTime(${data.id})">Excluir</button>`;
-                html += `</td>`;
-                html += `</tr>`;
-            });
-
-            $("#tableTimesBody").html(html);
-
-            // Inicialização da Tabela
-            $('#tableTimes').DataTable();
-        },
-        error: function () {
-            alert('Erro ao carregar a lista de times!');
-        }
-    });
-}
-
-function preencherModal(data, id) {
-    data = JSON.parse(decodeURIComponent(data));
-    console.log(data);
-    $("#nome").val(data.nome);
-    $("#dataCriacao").val(data.dataCriacao);
-    $("#cidade").val(data.cidade);
-    $("#pais").val(data.pais);
-    $("#numeroTorcedores").val(data.numeroTorcedores);
-    $("#tecnico").val(data.tecnico);
-    $("#dinheiroCaixa").val(data.dinheiroCaixa);
-    $("#nomeEstadio").val(data.nomeEstadio);
-    timeId = id;
-    console.log(timeId);
-}
-
-function limparModal() {
-    $("#nome").val('');
-    $("#dataCriacao").val('');
-    $("#cidade").val('');
-    $("#pais").val('');
-    $("#numeroTorcedores").val('');
-    $("#tecnico").val('');
-    $("#dinheiroCaixa").val('');
-    $("#nomeEstadio").val('');
-}
-
-$("#modalCadastro").click(limparModal);
-
-$(document).ready(function () {
+$(document).ready(function() {
+    // Função para carregar a lista de times ao carregar a página
     listarTimes();
-});
 
-var timeId = null;
-
-$("#salvarBotao").click(function (event) {
-    event.preventDefault();
-
-    var time = {
-        'nome': $("#nome").val(),
-        'dataCriacao': $("#dataCriacao").val(),
-        'cidade': $("#cidade").val(),
-        'pais': $("#pais").val(),
-        'numeroTorcedores': parseInt($("#numeroTorcedores").val()),
-        'tecnico': $("#tecnico").val(),
-        'dinheiroCaixa': parseFloat($("#dinheiroCaixa").val()),
-        'nomeEstadio': $("#nomeEstadio").val()
-    };
-
-    var url = 'http://localhost:8080/api/times';
-    var type = 'POST';
-
-    if (timeId !== null) {
-        url += '/' + timeId;
-        type = 'PUT';
-    }
-
-    $.ajax({
-        url: url,
-        type: type,
-        contentType: 'application/json',
-        data: JSON.stringify(time),
-        success: function () {
-            var message = timeId === null ? 'Time criado com sucesso!' : 'Time atualizado com sucesso!';
-            alert(message);
-            $('#myModal').modal('hide');
-            listarTimes();
-            timeId = null;  // Limpar o timeId após a operação bem-sucedida
-        },
-        error: function () {
-            var message = timeId === null ? 'Erro ao criar o time!' : 'Erro ao atualizar o time!';
-            alert(message);
-        }
-    });
-});
-
-function removerTime(id) {
-    if (confirm('Tem certeza de que deseja remover este time?')) {
+    // Função para carregar a lista de times
+    function listarTimes() {
         $.ajax({
-            url: 'http://localhost:8080/api/times/' + id,
-            type: 'DELETE',
+            url: 'http://localhost:8080/api/times',
+            type: 'GET',
+            dataType: 'json',
             success: function (result) {
-                alert('Time removido com sucesso!');
-                listarTimes();
+                console.log(result);
+                var html = '';
+                $.each(result, function (_i, data) {
+                    html += `<tr>`;
+                    html += `<td>${data.nome}</td>`;
+                    html += `<td>${data.dataCriacao}</td>`;
+                    html += `<td>${data.cidade}</td>`;
+                    html += `<td>${data.pais}</td>`;
+                    html += `<td>${data.numeroTorcedores}</td>`;
+                    html += `<td>${data.tecnico}</td>`;
+                    html += `<td>${data.dinheiroCaixa}</td>`;
+                    html += `<td>${data.nomeEstadio}</td>`;
+                    html += `<td>`;
+                    html += `<button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onclick="preencherModal('${encodeURIComponent(JSON.stringify(data))}', ${data.id})">Editar</button>`;
+                    html += `<button class="btn btn-sm btn-danger ml-1" onclick="removerTime(${data.id})">Excluir</button>`;
+                    html += `</td>`;
+                    html += `</tr>`;
+                });
+
+                $("#corpoTabela").html(html);
+
+                // Inicialização da Tabela
+                if ($.fn.dataTable.isDataTable('#tabelaTimes')) {
+                    $('#tabelaTimes').DataTable().clear().destroy();
+                }
+                $('#tabelaTimes').DataTable();
             },
-            error: function () {
-                alert('Erro ao remover o time!');
+            error: function (xhr, status, error) {
+                mostrarErro('Erro ao carregar a lista de times!', status, error);
             }
         });
     }
-}
+
+    // Função para preencher o modal com os dados do time
+    window.preencherModal = function(data, id) {
+        data = JSON.parse(decodeURIComponent(data));
+        $("#nome").val(data.nome);
+        $("#dataCriacao").val(data.dataCriacao);
+        $("#cidade").val(data.cidade);
+        $("#pais").val(data.pais);
+        $("#torcedores").val(data.numeroTorcedores);
+        $("#tecnico").val(data.tecnico);
+        $("#saldo").val(data.dinheiroCaixa);
+        $("#nomeEstadio").val(data.nomeEstadio);
+        timeId = id;
+    }
+
+    // Função para limpar o modal
+    function limparModal() {
+        $("#nome").val('');
+        $("#dataCriacao").val('');
+        $("#cidade").val('');
+        $("#pais").val('');
+        $("#torcedores").val('');
+        $("#tecnico").val('');
+        $("#saldo").val('');
+        $("#nomeEstadio").val('');
+    }
+
+    $("#modalCadastro").click(limparModal);
+
+    var timeId = null;
+
+    // Função para salvar ou atualizar um time
+    $("#salvarBotao").click(function(event) {
+        event.preventDefault();
+
+        var time = {
+            'nome': $("#nome").val(),
+            'dataCriacao': $("#dataCriacao").val(),
+            'cidade': $("#cidade").val(),
+            'pais': $("#pais").val(),
+            'numeroTorcedores': parseInt($("#torcedores").val()),
+            'tecnico': $("#tecnico").val(),
+            'dinheiroCaixa': parseFloat($("#saldo").val()),
+            'nomeEstadio': $("#nomeEstadio").val()
+        };
+
+        var url = 'http://localhost:8080/api/times';
+        var type = 'POST';
+
+        if (timeId !== null) {
+            url += '/' + timeId;
+            type = 'PUT';
+        }
+
+        $.ajax({
+            url: url,
+            type: type,
+            contentType: 'application/json',
+            data: JSON.stringify(time),
+            success: function () {
+                var message = timeId === null ? 'Time criado com sucesso!' : 'Time atualizado com sucesso!';
+                alert(message);
+                $('#myModal').modal('hide');
+                listarTimes();
+                timeId = null;  // Limpar o timeId após a operação bem-sucedida
+            },
+            error: function (res) {
+                var message = timeId === null ? 'Erro ao criar o time!' : 'Erro ao atualizar o time!';
+                console.log (res.responseText);
+                alert(message + " - " + res.responseText);
+            }
+        });
+    });
+
+    // Função para remover um time
+    window.removerTime = function(id) {
+        if (confirm('Tem certeza de que deseja remover este time?')) {
+            $.ajax({
+                url: 'http://localhost:8080/api/times/' + id,
+                type: 'DELETE',
+                success: function () {
+                    alert('Time removido com sucesso!');
+                    listarTimes();
+                },
+                error: function (xhr, status, error) {
+                    mostrarErro('Erro ao remover o time!', status, error);
+                }
+            });
+        }
+    }
+
+    // Função para exibir mensagem de erro detalhada
+    function mostrarErro(mensagem, status, error) {
+        var detalhes = '';
+        if (status) {
+            detalhes += '\nStatus: ' + status;
+        }
+        if (error) {
+            detalhes += '\nErro: ' + error;
+        }
+        alert(mensagem + detalhes);
+    }
+});
